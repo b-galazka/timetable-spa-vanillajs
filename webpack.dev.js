7c1361f4413517834353d710a61a31e45b9a29e5
b-galazka <bartlomiejgalazka@zoho.eu>
2018-02-10 18:05:15 +0100

    initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+
+    entry: [
+        'babel-polyfill',
+        path.resolve(__dirname, 'src/index.js')
+    ],
+
+    output: {
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: 'app.js',
+        publicPath: '/dist/'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                include: path.resolve(__dirname, 'src'),
+                loader: 'babel-loader'
+            },
+
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    { loader: 'css-loader', options: { importLoaders: 1 } },
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
+    resolve: {
+        extensions: ['.js', '.json']
+    },
+
+    devServer: {
+        contentBase: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'dist')],
+        inline: true,
+        hot: true,
+        port: 3000,
+        historyApiFallback: true,
+        publicPath: '/js/'
+    }
+};
---
+++ webpack.prod.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractCSS = new ExtractTextPlugin('../css/style.css');
+
+module.exports = {
+
+    entry: [
+        'babel-polyfill',
+        path.resolve(__dirname, 'src/index.js')
+    ],
+
+    output: {
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: 'app.js',
+        publicPath: '/dist/'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                include: path.resolve(__dirname, 'src'),
+                loader: 'babel-loader'
+            },
+
+            {
+                test: /\.scss$/,
+                use: extractCSS.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        { loader: 'css-loader', options: { importLoaders: 1 } },
+                        'postcss-loader',
+                        'sass-loader'
+                    ]
+                })
+            }
+        ]
+    },
+
+    plugins: [
+        extractCSS
+    ],
+
+    resolve: {
+        extensions: ['.js', '.json']
+    }
+};
