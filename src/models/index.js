424d22250196882d8fd336974ca4e7851e3a1610
b-galazka <bartlomiejgalazka@zoho.eu>
2018-02-11 11:54:10 +0100

    add UI model

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,4 @@
 export * from './mobileApp';
 export * from './timetableObject';
 export * from './timetableObjectsLists';
+export * from './userInterface';
---
+++ src/models/userInterface.js
@@ -0,0 +1,7 @@
+import { Model } from '../lib/Model';
+
+class UserInterfaceModel extends Model {
+
+}
+
+export const userInterfaceModel = new UserInterfaceModel();
